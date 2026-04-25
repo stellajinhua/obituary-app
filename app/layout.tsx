fdcf036b004687c841cbdc3170232f3b32008227
Stella Chua <all3ts@gmail.com>
2026-04-25 16:20:29 +0800

    fix IOS app icon, updating the file name

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,11 +32,11 @@ export const metadata: Metadata = {
   // ✅ Icons (important)
   icons: {
     icon: [
-      { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/apple-touch-icon.png", sizes: "192x192", type: "image/png" },
       { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: [
-      { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/apple-touch-icon.png", sizes: "192x192", type: "image/png" },
     ],
   },
 
